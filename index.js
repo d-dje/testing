321a700a9187ecea55cc3173863d505b808e64cc
d-dje <dalila.djebali@gmail.com>
2017-10-25 01:58:16 -0400

    Removed useless line feeds

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const compose = f => g => x => f(g(x));
-
 const add = a => b => a + b;
 const multiply = a => b => a * b;
 const add2 = add(2);
 const multiplyBy3 = multiply(3);
-
 const add2ThenMultiplyBy3 = compose(multiplyBy3)(add2);
 
 console.log(add2ThenMultiplyBy3(10));
